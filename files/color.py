1fc932517d53b24ffe1fe6ab6e32215941aecabf
agung xp <91707718+Gitcomeon8@users.noreply.github.com>
2021-10-03 20:42:34 +0700

    Create color.py

---
+++ files/color.py
@@ -0,0 +1,12 @@
+#!/color!
+#-*- coding:utf -8-*-
+
+class Color:
+    white = '\033[1;37m' # White bold
+    whiteNormal = '\033[0m'  # white (Normal)
+    red = '\033[31m' # red
+    green = '\033[1;32m' # green bold
+    orange = '\033[33m' # orange
+    blue = '\033[34m' # blue
+    purple = '\033[35m' # purple
+    cyan = '\033[36m' # cyan
